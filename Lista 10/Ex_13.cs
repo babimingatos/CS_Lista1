3da2dc1304bdb3251eedc2d19fc201223af0bc19
Barbara Mingatos <72284498+babimingatos@users.noreply.github.com>
2022-12-09 23:23:31 -0300

    Create Ex_13.cs

---
+++ Lista 10/Ex_13.cs
@@ -0,0 +1,53 @@
+static void Main(string[] args)
+        {
+            int[] tam = new int[5];
+            int[] par = new int[tam.Length];
+            int[] impar = new int[tam.Length];
+
+            for (int i = 0; i < tam.Length; i++)
+            {
+                Console.WriteLine("Digite os números: ");
+                int num = int.Parse(Console.ReadLine());
+
+                tam[i] = num;
+                while (tam[i] == 0)
+                {
+                    Console.Write("Erro! Digite um número maior.");
+                    Console.WriteLine("Digite um número maior que zero: ");
+                    num = int.Parse(Console.ReadLine());
+                    tam[i] = num;
+                    break;
+
+                }
+            }
+
+
+            int somaPar = 0;
+
+            for (int i = 0; i < tam.Length; i++)
+            {
+                if (tam[i] % 2 == 0)
+                {
+                    par[i]++;
+                    somaPar += par[i];
+
+                }
+
+            }
+            Console.WriteLine("\nPares: " + somaPar);
+
+            int somaImpar = 0;
+            for (int i = 0; i < tam.Length; i++)
+            {
+                if (tam[i] % 2 != 0)
+                {
+                    impar[i]++;
+                    somaImpar += impar[i];
+
+
+                }
+            }
+            Console.WriteLine("\nÍmpar: " + somaImpar);
+
+            
+        }
